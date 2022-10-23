d7fa14d04f987147b84f113262fb85759854307e
S.Smirnov <s.smirnov@yadro.com>
2022-10-23 19:26:29 +0300

    vite build fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HeaderComponent from "./components/Header.vue";
+import HeaderComponent from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import Main from "@/components/main/Main.vue";
 import Carousel from "@/components/Carousel.vue";
